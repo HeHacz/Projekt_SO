685fe521b5e988eb3bcf7f034a1bfd4bfb466171
HeHacz <hehahiyourip@gmail.com>
2017-06-18 18:18:40 +0200

    Poprawiono działanie wątków

--- main.cpp
+++ main.cpp
@@ -45,13 +45,16 @@
 #include<sys/un.h>
 #include <SFML/Network.hpp>
 #include<mutex>
-
+#include<condition_variable>
+#include<atomic>
+#include<ncurses.h>
 
 #define SA (struct sockaddr*)
 struct Wynik
 {
   std::string adres_IP[30];
 };
+
 std::vector<char*> adresy_IP;
 std::vector<char*> zywe_adresy_IP;
 std::vector<int> porty;
@@ -59,14 +62,22 @@ std::vector<int> otwarte_porty;
 std::vector<Wynik> wynik_tracerouta;
 std::vector<char*> martwe_adresy_IP;
 std::vector<int> zablokowane_porty;
+std::vector<char*> portable_IP;
+std::mutex mutex_;
+std::mutex mutex__;
+std::mutex mutex___;
 std::mutex adresy_IPGuard;
 std::mutex zywe_adresy_IPGuard;
+std::mutex portable_IPGuard;
 std::mutex martwe_adresy_IPGuard;
 std::mutex portyGuard;
 std::mutex otwarte_portyGuard;
 std::mutex wynik_traceroutaGuard;
 std::mutex zablokowane_portyGuard;
-
+std::condition_variable cv1;
+std::condition_variable cv2;
+std::condition_variable cv3;
+std::condition_variable cv4;
 
 auto generator_liczb_losowych(int koniec)
 {
@@ -112,7 +123,7 @@ int ping(char* adres_IP)
     }
   int status = ping_send(ping);
   const char *count = ping_get_error (ping);
-  std::cout<<"status> "<<status<<" , count: "<<count<<std::endl;
+  std::cout<<"status> "<<status<<" , count: "<<count<<adres_IP<<std::endl;
   if (status == -EINTR)
       {
 	std::cout<<status<<std::endl;
@@ -141,10 +152,9 @@ std::string* traceroute(char *adres_IP)
   char buf[4096] = { 0 };
   struct ip *ip_hdr = (struct ip *) buf;
   int hop = 0;
-
+    
   std::string* wynik = new std::string[30];
-  
-  int one = 1;
+   int one = 1;
   const int *val = &one;
   if (setsockopt (sfd, IPPROTO_IP, IP_HDRINCL, val, sizeof (one)) < 0)
     printf ("Cannot set HDRINCL!\n");
@@ -193,7 +203,6 @@ std::string* traceroute(char *adres_IP)
 		  inet_ntoa (addr2.sin_addr), hop);
 	  return wynik;
 	}
-
       hop++;
     }
 
@@ -206,29 +215,101 @@ static bool skaner_portow(int port , char *adres_IP)
   return (sf::TcpSocket().connect(adres_IP, port) == sf::Socket::Done);
 }
 
+bool martwe_adresy_IP_Is_empty()
+{
+  if(martwe_adresy_IP.empty())
+    return false;
+  else
+    return true;
+}
+
+bool zywe_adresy_IP_Is_empty()
+{
+  if(zywe_adresy_IP.empty())
+    return false;
+  else
+    return true;
+}
+
+bool adresy_IP_Is_No_empty()
+{
+  if(adresy_IP.empty())
+    return false;
+  else
+    return true;
+}
+
+bool adresy_IP_Is_empty()
+{
+  if(adresy_IP.empty())
+    return true;
+  else
+    return false;
+  
+}
+
+bool porty_Is_empty()
+{
+  if (porty.empty())
+    return true;
+  else
+    return false;
+}
+
+bool portable_IP_Is_empty()
+{
+  if(portable_IP.empty()&&!porty_Is_empty())
+    return false;
+  else
+    return true;
+}
+
+
 void losuj_IPThread()
 {
-  adresy_IPGuard.lock();
+  std::cout<< "IP"<<std::endl;
+  std::unique_lock<std::mutex> lock(zywe_adresy_IPGuard);
+  cv3.wait(lock, adresy_IP_Is_empty);
+  
+  //std::this_thread::sleep_for(std::chrono::seconds(2));
+  //adresy_IPGuard.lock();
+  std::lock_guard<std::mutex> lk_1(adresy_IPGuard);
+  std::this_thread::sleep_for(std::chrono::milliseconds(110));
   adresy_IP.push_back(losuj_IP());
-  adresy_IPGuard.unlock();
+  //adresy_IPGuard.unlock();
+  cv1.notify_one();
 }
 
 void losuj_PortThread()
 {
+  std::cout<< "port"<<std::endl;
+  
+  //std::this_thread::sleep_for(std::chrono::seconds(2));
   portyGuard.lock();
+  //std::this_thread::sleep_for(std::chrono::seconds(2));
+  
   porty.push_back(losuj_port());
   portyGuard.unlock();
+  cv2.notify_one();
 }
 
 void pingThread()
 {
-  zywe_adresy_IPGuard.lock();
-  adresy_IPGuard.lock();
-  martwe_adresy_IPGuard.lock();
-
-  if (ping(adresy_IP.back()) < 0)
+ 
+  std::cout<< "ping"<<std::endl;
+  auto now = std::chrono::system_clock::now();
+  std::unique_lock<std::mutex> lock(mutex_);
+  cv1.wait(lock);
+  std::lock(adresy_IPGuard, zywe_adresy_IPGuard, martwe_adresy_IPGuard, portable_IPGuard);
+  std::lock_guard<std::mutex> lk_1(adresy_IPGuard, std::adopt_lock);
+  std::lock_guard<std::mutex> lk_2(zywe_adresy_IPGuard, std::adopt_lock);
+  std::lock_guard<std::mutex> lk_3(martwe_adresy_IPGuard, std::adopt_lock);
+  std::lock_guard<std::mutex> lk_4(portable_IPGuard, std::adopt_lock);
+  
+  if (ping(adresy_IP.back()) > 0)
     {
       zywe_adresy_IP.push_back(adresy_IP.back());
+      portable_IP.push_back(adresy_IP.back());
       adresy_IP.pop_back();
     }
   else
@@ -236,16 +317,23 @@ void pingThread()
       martwe_adresy_IP.push_back(adresy_IP.back());
       adresy_IP.pop_back();
     }
-  
-  martwe_adresy_IPGuard.unlock();
-  adresy_IPGuard.unlock();
-  zywe_adresy_IPGuard.unlock();
+  cv2.notify_one();
+  cv3.notify_one();
 }
 
 void tracertThread()
 {
-  zywe_adresy_IPGuard.lock();
-  wynik_traceroutaGuard.lock();
+  std::cout<< "tracert"<<std::endl;
+  auto now = std::chrono::system_clock::now();
+  std::unique_lock<std::mutex> lock(mutex__);
+  // std::unique_lock<std::mutex> lock(mutex4);
+  cv2.wait_until(lock, now + std::chrono::seconds(20) , zywe_adresy_IP_Is_empty);
+  // cv5.wait(lock);
+  std::lock(zywe_adresy_IPGuard, wynik_traceroutaGuard);
+  std::lock_guard<std::mutex> lk_1(zywe_adresy_IPGuard, std::adopt_lock);
+  std::lock_guard<std::mutex> lk_2(wynik_traceroutaGuard, std::adopt_lock);
+  if(zywe_adresy_IP_Is_empty())
+    {
   std::string *wyniki, *cos;
   Wynik w;
   wyniki = traceroute(zywe_adresy_IP.back());
@@ -257,57 +345,131 @@ void tracertThread()
      i++;
     }
   cos = new std::string[i];
-  for (int j=0;j<i; j++)
+  for (int j=1;j<i+1; j++)
     {
       w.adres_IP[j] = wyniki[j];
-      
-      //w.adres_IP = &cos;
-  std::cout<< j <<" == " <<w.adres_IP[j] << " == " << wyniki[j]<<std::endl;
+      std::cout<< j <<" == " <<w.adres_IP[j] << " == " << wyniki[j]<<std::endl;
     }
   
   wynik_tracerouta.push_back(w);
-  wynik_traceroutaGuard.unlock();
-  zywe_adresy_IPGuard.unlock();
+    }
 }
 
 void skan_portowThread()
 {
-  zywe_adresy_IPGuard.lock();
-  portyGuard.lock();
-  otwarte_portyGuard.lock();
-  zablokowane_portyGuard.lock();
-  if(skaner_portow(porty.back(), zywe_adresy_IP.back()))
+  std::cout<< "skan_port"<<std::endl;
+  auto now = std::chrono::system_clock::now();
+  std::unique_lock<std::mutex> lock(zywe_adresy_IPGuard);
+  cv4.wait_until(lock, now + std::chrono::seconds(30) ,portable_IP_Is_empty);
+  std::lock(portable_IPGuard, portyGuard, otwarte_portyGuard, zablokowane_portyGuard);
+  
+  std::lock_guard<std::mutex> lk_1(portable_IPGuard, std::adopt_lock);
+  std::lock_guard<std::mutex> lk_2(portyGuard, std::adopt_lock);
+  std::lock_guard<std::mutex> lk_3(otwarte_portyGuard, std::adopt_lock);
+  std::lock_guard<std::mutex> lk_4(zablokowane_portyGuard, std::adopt_lock);
+  if(portable_IP_Is_empty())
+    {
+  if(skaner_portow(porty.back(), portable_IP.back()))
     {
       otwarte_porty.push_back(porty.back());
       porty.pop_back();
+      portable_IP.pop_back();
+      std::cout<<"open: "<<otwarte_porty.back()<<std::endl;
+      
     }
     else
     {
       zablokowane_porty.push_back(porty.back());
       porty.pop_back();
+      portable_IP.pop_back();
+      std::cout<<"close: "<<zablokowane_porty.back()<<std::endl;
+    }
+  std::cout<<"koniec"<<std::endl;
+  //cv5.notyfy_one;
     }
 }
 
+auto ncurses()
+{
+  initscr();
+  printw ( "Hello World !!!" );
+  getch();
+  endwin();
+  return 0;
+}
 
 int main()
 {
-  zywe_adresy_IP.push_back("8.8.8.8");
-  
+  //zywe_adresy_IP.push_back("8.8.8.8");
+  int lw = 11;
   std::string *wynik;
-  char *adres_IP = losuj_IP();
+  auto curses = std::thread(ncurses);
+  std::vector<std::thread> threada;
+  for(int i = 0; i < lw; i++)
+    {
+      threada.push_back(std::thread(losuj_IPThread));
+    }
+  std::vector<std::thread> threadb;
+  for(int i = 0; i < lw; i++)
+    {
+      threadb.push_back(std::thread(losuj_PortThread));
+    }
+  std::vector<std::thread> threadc;
+  for(int i = 0; i < lw; i++)
+    {
+      threadc.push_back(std::thread(pingThread));
+    }
+    std::vector<std::thread> threadd;
+  for(int i = 0; i < lw; i++)
+    {
+      threadd.push_back(std::thread(tracertThread));
+    }
+  std::vector<std::thread> threade;
+  for(int i = 0; i < lw; i++)
+    {
+      threade.push_back(std::thread(skan_portowThread));
+    }
+  
+  for(auto& thread : threada)
+    {
+      thread.join();
+    }
+  for(auto& thread : threadb)
+       {
+	 thread.join();
+       }
+  for(auto& thread : threadc)
+    {
+      thread.join();
+    }
+  for(auto& thread : threadd)
+    {
+      thread.join();
+      }
+  for(auto& thread : threade)
+    {
+      thread.join();
+      }
+  
+  curses.join();
+  
+  
+  
+  
+  
+  
   //std::cout<<adres_IP<<std::endl<<losuj_port()<<std::endl<<"ping: "<<ping(adres_IP)<<std::endl;
 //std::cout<<skaner_portow(losuj_port(), adres_IP)<<std::endl<<"google.cos: 80 \
 //"<<skaner_portow(53,"8.8.8.8")<<std::endl;
 
- tracertThread();
- int a = 0;
- for(auto const& i: wynik_tracerouta)
-   {
+// tracertThread();
+  //int a = 0;
+  //for(auto const& i: wynik_tracerouta)
+  /*{
      std::cout<< a << ": "<< i.adres_IP[a] <<std::endl;
      a++;
    }
- 
- //std::cout<<traceroute(adres_IP)<<std::endl;
-std::cout<<"hasło okoń"<<std::endl;
+  //std::cout<<traceroute(adres_IP)<<std::endl;
+ */std::cout<<"hasło okoń"<<std::endl;
 return 0;
 }
